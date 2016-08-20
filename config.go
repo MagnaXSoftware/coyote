ce870c9c9ec18764d6469e86352be6424815f756
t0xicCode <xavier.l@magnax.ca>
2016-08-20 13:55:54 -0400

    Updated to the v1.1.1 agreement

--- config.go
+++ config.go
@@ -33,7 +33,7 @@ func init() {
 	acmeServerURL := flag.String("acme-server", "https://acme-v01.api.letsencrypt.org/directory", "URL of the ACME server directory. defaults to the Let's Encrypt live server")
 	accountKeyPath := flag.String("account-key", "", "Path to your Let's Encrypt account private key.")
 	flag.StringVar(&Config.AccountEmail, "account-email", "", "The email to associate with the registration.")
-	flag.StringVar(&Config.AccountTerms, "account-terms", "https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf", "The terms that need to be accepted before certificate issuance.")
+	flag.StringVar(&Config.AccountTerms, "account-terms", "https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf", "The terms that need to be accepted before certificate issuance.")
 	flag.StringVar(&Config.ChallengeDir, "challenge-dir", ".well-known/acme-challenge/", "Path to the challenge directory.")
 	csrPath := flag.String("csr", "", "Path to your CSR file.")
 	flag.StringVar(&Config.CertificatePath, "cert", "", "Path to the certificate file (with chain)")
